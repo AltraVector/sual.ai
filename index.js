2569cf84a6158f584ffaf089341a41712cfb6c06
AltraVector <169048640+AltraVector@users.noreply.github.com>
2024-05-06 21:15:04 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express, { urlencoded } from "express";
 import dotenv from 'dotenv'
-import cors from 'cors'
+//import cors from 'cors'
 import { Post_Text } from "./src/views/api.js";
 
 const app = express()
@@ -8,7 +8,7 @@ const app = express()
 
 const PORT = process.env.PORT || 5000
 app.use(express.json())
-app.use(cors())
+//app.use(cors())
 
 dotenv.config()
 
